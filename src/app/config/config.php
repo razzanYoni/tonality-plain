6c47cdc1bebb487fe1524b838ec0c4dbb155459c
razzanYoni <13521087@mahasiswa.itb.ac.id>
2023-09-28 15:57:15 +0700

    add config file

---
+++ src/app/config/config.php
@@ -0,0 +1,29 @@
+<?php
+
+// URL
+define('BASE_URL', 'http://localhost:8080/public');
+define('STORAGE_URL', 'http://localhost:8080/storage');
+
+// Database
+define('HOST', $_ENV['DB_HOST']);
+define('USER', $_ENV['MYSQL_USER']);
+define('PASSWORD', $_ENV['MYSQL_PASSWORD']);
+define('DATABASE', $_ENV['MYSQL_DATABASE']);
+define('PORT', $_ENV['MYSQL_PORT']);
+define('ROWS_PER_PAGE', 10); // Application Logic
+
+// File
+// define("ROOT_DIR", __DIR__ . "/");
+define('DEFAULT_FILE_PATH', 'files/');
+define('MAX_SIZE', 10 * 1024 * 1024); //10MB
+
+// Bcrypt
+define('BCRYPT_COST', 12);
+
+// Session
+define('COOKIES_LIFETIME', 24 * 60 * 60);
+define('SESSION_EXPIRATION_TIME', 24 * 60 * 60);
+define('SESSION_REGENERATION_TIME', 30 * 60);
+
+// Debounce
+define('DEBOUNCE_TIMEOUT', 300);
