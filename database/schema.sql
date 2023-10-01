f448390f0d950dec7fa88464d717730502a94500
Noel Simbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-10-01 22:35:18 +0700

    feat: change key data types to int

--- database/schema.sql
+++ database/schema.sql
@@ -1,44 +1,45 @@
 create table if not exists users
 (
-    user_id  serial primary key,
+    user_id  int auto_increment primary key,
     username varchar(50)  not null,
     password varchar(255) not null,
     is_admin boolean default false
-) CHARACTER SET utf8;
+) character set utf8;
 
 create table if not exists albums
 (
-    album_id     serial primary key,
+    album_id     int auto_increment primary key,
     album_name   varchar(255) not null,
     release_date date         not null,
     genre        varchar(50)  not null,
     artist       varchar(255) not null,
     cover_url    varchar(255) not null
-) CHARACTER SET utf8;
+) character set utf8;
 
 create table if not exists songs
 (
-    song_id     serial primary key,
-    album_id    serial references albums,
+    song_id     int auto_increment primary key,
+    album_id    int references albums,
     title       varchar(255) not null,
     artist      varchar(255) not null,
     song_number smallint     not null,
     disc_number smallint,
     duration    integer      not null,
     audio_url   varchar(255) not null
-) CHARACTER SET utf8;
+) character set utf8;
 
 create table if not exists playlists
 (
-    playlist_id   serial primary key,
-    user_id       varchar(36)  not null references users,
+    playlist_id   int auto_increment primary key,
+    user_id       int          not null references users,
     playlist_name varchar(50)  not null,
     description   text,
     cover_url     varchar(255) not null
-) CHARACTER SET utf8;
+) character set utf8;
 
 create table if not exists appears_on
 (
-    song_id     serial primary key references songs,
-    playlist_id serial primary key references playlists
-) CHARACTER SET utf8;
+    song_id     int references songs,
+    playlist_id int references playlists,
+    primary key (song_id, playlist_id)
+) character set utf8;
