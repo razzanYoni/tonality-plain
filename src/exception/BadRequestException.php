e5880b70682a6768b10fc4b59d4bf4fd43382f64
razzanYoni <13521087@mahasiswa.itb.ac.id>
2023-10-02 17:09:42 +0700

    rename folder

--- src/exception/BadRequestException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class BadRequestException extends Exception
-{
-  public function __construct($message = "", $code = 0, Throwable $previous = null)
-  {
-    parent::__construct($message, $code, $previous);
-  }
-}
