9afaeb5c0e61a410190c6244eaddb3e6bfffcdf7
razzanYoni <13521087@mahasiswa.itb.ac.id>
2023-10-02 20:52:59 +0700

    feat: Base service class

---
+++ src/bases/BaseService.php
@@ -0,0 +1,16 @@
+<?php
+
+abstract class BaseSrv {
+  protected static $instance;
+  protected $repository;
+
+  protected function __construct() {
+  }
+
+  public static function getInstance() {
+    if (!isset(self::$instance)) {
+      self::$instance = new static();
+    }
+    return self::$instance;
+  }
+}
