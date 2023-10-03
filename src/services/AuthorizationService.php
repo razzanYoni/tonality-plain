6a6393cb5a96293641d0198178a2e9ff2e7d59b8
Noel Simbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-10-03 15:10:04 +0700

    refactor: add type hinting in services

--- src/services/AuthorizationService.php
+++ src/services/AuthorizationService.php
@@ -4,39 +4,47 @@ namespace services;
 
 use bases\BaseService;
 
-class AuthService extends BaseService {
-    private static $instance;
-    private $userService;
+class AuthorizationService extends BaseService
+{
+    protected static BaseService $instance;
+    private BaseService $userService;
 
-    private function __construct() {
+    private function __construct()
+    {
         $this->userService = UserService::getInstance();
     }
 
-    public static function getInstance() : AuthService {
+    public static function getInstance(): AuthorizationService
+    {
         if (!isset(self::$instance)) {
             self::$instance = new static();
         }
         return self::$instance;
     }
 
-    public function isLogin() {
+    public function isLogin(): bool
+    {
         return isset($_SESSION['user_id']);
     }
 
-    public function isAdmin() {
+    public function isAdmin(): bool
+    {
         return $this->isLogin() && $_SESSION['is_admin'];
     }
 
-    public function getUserService() {
+    public function getUserService(): BaseService|UserService
+    {
         return $this->userService;
     }
 
-    public function logout() {
+    public function logout(): void
+    {
         unset($_SESSION['user_id']);
         unset($_SESSION['is_admin']);
     }
 
-    public function getCurrentUser() {
+    public function getCurrentUser()
+    {
         if (!$this->isLogin()) {
             return null;
         }
