878e22518a1437c03b1245b2b2a7d649cf92bd01
Noel Simbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-10-03 15:25:37 +0700

    style: add type hinting in BaseController

--- src/bases/BaseController.php
+++ src/bases/BaseController.php
@@ -3,35 +3,55 @@
 namespace bases;
 
 use exceptions;
+use exceptions\MethodNotAllowedException;
 
-abstract class BaseController {
-    protected static $instance;
-    protected $service;
+abstract class BaseController
+{
+    protected static BaseController $instance;
+    protected BaseService $service;
 
-    protected function __construct($service) {
+    protected function __construct($service)
+    {
         $this->service = $service;
     }
-    
-    public static function getInstance($service) {
+
+    public static function getInstance($service): BaseController|static
+    {
         if (!isset(self::$instance)) {
             self::$instance = new static($service);
         }
         return self::$instance;
     }
 
-    protected function get($urlParameters) {
+    /**
+     * @throws MethodNotAllowedException
+     */
+    protected function get($urlParameters)
+    {
         throw new exceptions\MethodNotAllowedException("Method Not Allowed");
     }
 
-    protected function post($urlParameters) {
+    /**
+     * @throws MethodNotAllowedException
+     */
+    protected function post($urlParameters)
+    {
         throw new exceptions\MethodNotAllowedException("Method Not Allowed");
     }
 
-    protected function put($urlParameters) {
+    /**
+     * @throws MethodNotAllowedException
+     */
+    protected function put($urlParameters)
+    {
         throw new exceptions\MethodNotAllowedException("Method Not Allowed");
     }
 
-    protected function delete($urlParameters) {
+    /**
+     * @throws MethodNotAllowedException
+     */
+    protected function delete($urlParameters)
+    {
         throw new exceptions\MethodNotAllowedException("Method Not Allowed");
     }
 }
