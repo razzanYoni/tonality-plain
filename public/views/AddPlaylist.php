4f68475f63086d6ed842d523fc8da68ecbf66c6a
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-10-03 09:54:39 +0700

    fix: form add playlist

--- public/views/AddPlaylist.php
+++ public/views/AddPlaylist.php
@@ -3,39 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="../js/FormDetail.js"></script>
-    <link rel="stylesheet" href="../css/FormDetail.css">
-    <link href="https://fonts.googleapis.com/css?family=Inter" rel="stylesheet">
+    <script src="/public/js/FormDetail.js"></script>
+    <link rel="stylesheet" href="/public/css/FormDetail.css" type="text/css">
+    <!-- <link href="/public/assets/fonts" rel="stylesheet"> -->
     <title>Form</title>
 </head>
 <body>
     <div class="form-container">
         <h1>Add New Playlist</h1>
-        <div class="form-list">
+        <form class="form-list">
             <div class="playlist-quest">
                 <div class="form-label">Playlist Name</div>
-                <form>
                     <input type="text">
-                </form>
             </div>
             <div class="playlist-quest">
                 <div class="form-label">Description</div>
-                <form>
                     <input type="text">
-                </form>
             </div>
             <div class="playlist-quest">
                 <div class="form-label">Cover Photo</div>
-                <form>
                     <input type="file" id="input-file" accept="image/*">
                     <label for="input-file" class="custom-file-upload" id="file-label">Choose Your Playlist Cover</label>
-                </form>
             </div>
             <div class="cancel-submit">
                 <button class="cancel-btn">Cancel</button>
                 <button class="add-btn">Add Song</button>
             </div>
-        </div>
+        </form>
     </div>
 </body>
 </html>
