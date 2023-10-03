18795c7eeb0a57c75aed8402ea09f7486f232a19
Noel Simbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-10-03 17:36:50 +0700

    refactor: CheckUsernameController

--- src/controllers/auth/CheckUsernameController.php
+++ src/controllers/auth/CheckUsernameController.php
@@ -1,32 +1,33 @@
 <?php
 
+namespace controllers\auth;
+
 use bases\BaseController;
+use bases\BaseService;
 use services\UserService;
 
-class CheckUsernameController extends BaseController {
-    private static $instance;
+class CheckUsernameController extends BaseController
+{
+    /* @var UserService $service */
+    protected BaseService $service;
 
-    private function __construct($userService) {
-        parent::__construct($userService);
-    }
-    
-    public static function getInstance() : CheckUsernameController {
+    public static function getInstance($service): CheckUsernameController
+    {
         if (!isset(self::$instance)) {
             self::$instance = new static(UserService::getInstance());
         }
         return self::$instance;
     }
 
-    public function post($urlParameters) {
+    public function post($urlParameters): void
+    {
         $username = $_POST['username'];
-        $user = $this->service->isUsernameExists($username);
+        $user = $this->service->isUsernameExist($username);
 
         http_response_code(200);
         echo json_encode(array(
             'is_exists' => $user != null,
             'message' => $user != null ? 'Username already exists' : 'Username is available'
         ));
-
-        return;
     }
 }
