df53076af288661e3e6b3f1fe84a8f364902c617
razzanYoni <13521087@mahasiswa.itb.ac.id>
2023-10-04 15:57:38 +0700

    add authorization view

--- public/views/Login.php
+++ public/views/Login.php
@@ -1,32 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="/public/js/Login.js"></script>
-    <link rel="stylesheet" href="/public/css/Login.css" type="text/css">
-    <title>Login</title>
-</head>
-<body>
-    <div class="login-container">
-        <img class="logo" src="/public/assets/icons/logo.jpeg"></img>
-        <div class="login-title">Log in for Tonality</div>
-        <form class="login-form">
-            <div class="login-quest">
-                <div class="form-label">Username</div>
-                <input type="text">
-            </div>
-            <div class="login-quest">
-                <div class="form-label">Password</div>
-                <input type="password">
-            </div>
-            <div class="btn-for-login">
-                <button class="login-btn">Log In</button>
-            </div>
-        </form>
-        <div class="sign-up-link">
-            Don't have an account? <a href="Register.php" class="signup-text">Sign up for Tonality</a>
-        </div>
-    </div>
-</body>
-</html>
+<?php
+
+/** @var $model \models\LoginForm */
+
+require_once ROOT_DIR . "src/forms/Form.php";
+
+use forms\Form;
+
+?>
+
+<h1>Login</h1>
+
+<?php $form = Form::begin('', 'post') ?>
+<?php echo $form->field($model, 'username') ?>
+<?php echo $form->field($model, 'password')->passwordField() ?>
+<button class="btn btn-success">Submit</button>
+<?php Form::end() ?>
--- public/views/Register.php
+++ public/views/Register.php
@@ -1,36 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="/public/js/Login.js"></script>
-    <link rel="stylesheet" href="/public/css/Login.css" type="text/css">
-    <title>Login</title>
-</head>
-<body>
-    <div class="login-container">
-        <img class="logo" src="/public/assets/icons/logo.jpeg"></img>
-        <div class="login-title">Sign up for Tonality</div>
-        <form class="register-form">
-            <div class="register-quest">
-                <div class="form-label">Username</div>
-                <input type="text">
-            </div>
-            <div class="register-quest">
-                <div class="form-label">Password</div>
-                <input type="password">
-            </div>
-            <div class="register-quest">
-                <div class="form-label">Confirm Password</div>
-                <input type="password">
-            </div>
-            <div class="btn-for-signup">
-                <button class="login-btn">Sign Up</button>
-            </div>
-        </form>
-        <div class="login-link">
-            Don't have an account? <a href="Login.php" class="login-text">Log in to Tonality</a>
-        </div>
-    </div>
-</body>
-</html>
+<?php
+
+/** @var $model \cores\Model */
+
+require_once ROOT_DIR . "src/forms/Form.php";
+
+use forms\Form;
+
+$form = new Form();
+?>
+
+<h1>Register</h1>
+
+<?php $form = Form::begin('', 'post') ?>
+<?php echo $form->field($model, 'username') ?>
+<?php echo $form->field($model, 'password')->passwordField() ?>
+<?php echo $form->field($model, 'passwordConfirm')->passwordField() ?>
+<button class="btn btn-success">Submit</button>
+<?php Form::end() ?>
