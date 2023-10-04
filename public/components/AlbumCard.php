2789d19ec1657c0ae15ce078a164183230b9857a
Noel Simbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-10-04 11:21:06 +0700

    component: add album card

---
+++ public/components/AlbumCard.php
@@ -0,0 +1,22 @@
+<?php
+
+use models\AlbumModel;
+
+function AlbumCard(AlbumModel $album): string
+{
+    $albumId = $album->get('album_id');
+    $coverUrl = $albumId->get('cover_url');
+    $albumName = $album->get('album_name');
+    $albumArtist = $album->get('artist');
+
+    return
+    <<<"EOT"
+    <a href="/album/$albumId" class="album-card">
+        <div class="album-info-container">
+            <img src=$coverUrl alt="album cover image" class="album-cover-image"/>
+            <div class="album-name">$albumName</div>
+            <div class="artist-name">$albumArtist</div>
+        </div>
+    </a>
+    EOT;
+}
