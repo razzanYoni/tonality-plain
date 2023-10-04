9743a2db3b4b30c8eee2007d9e28df80cea0ba3c
Noel Simbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-10-04 08:22:46 +0700

    style: reformat exceptions

--- src/exceptions/BadRequestException.php
+++ src/exceptions/BadRequestException.php
@@ -6,8 +6,8 @@ use Exception, Throwable;
 
 class BadRequestException extends Exception
 {
-  public function __construct($message = "", $code = 0, Throwable $previous = null)
-  {
-    parent::__construct($message, $code, $previous);
-  }
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
--- src/exceptions/MethodNotAllowedException.php
+++ src/exceptions/MethodNotAllowedException.php
@@ -6,8 +6,8 @@ use Exception, Throwable;
 
 class MethodNotAllowedException extends Exception
 {
-  public function __construct($message = "", $code = 0, Throwable $previous = null)
-  {
-    parent::__construct($message, $code, $previous);
-  }
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
