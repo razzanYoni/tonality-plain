8c0ffaf78a38d5554b76df20777fcc3764db9b4c
razzanYoni <13521087@mahasiswa.itb.ac.id>
2023-10-05 11:18:12 +0700

    rename file

--- public/views/layouts/blank.php
+++ public/views/layouts/blank.php
@@ -1,12 +1,8 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<!--    <link rel="stylesheet" href="/public/css/Login.css" type="text/css">-->
-
-    <!--    <title>Hello, world!</title>-->
 </head>
 <body>
 
