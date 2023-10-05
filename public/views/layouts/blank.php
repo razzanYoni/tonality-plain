848035de085393bf66a39ef9b20eaad45cc51d05
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-10-05 19:43:27 +0700

    feat: login and register

--- public/views/layouts/blank.php
+++ public/views/layouts/blank.php
@@ -2,12 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/public/css/LoginRegister.css" type="text/css">
+    <link rel="stylesheet" href="/public/css/NavBar.css" type="text/css">
 </head>
 <body>
 
-<div class="container">
     {{content}}
-</div>
+
 </body>
 </html>
