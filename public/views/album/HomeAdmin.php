39fcf8ad44fc6a56738aa179bc6ef689bcf03790
razzanYoni <13521087@mahasiswa.itb.ac.id>
2023-10-05 21:50:53 +0700

    chore : change font

--- public/views/album/HomeAdmin.php
+++ public/views/album/HomeAdmin.php
@@ -1,10 +1,7 @@
 <h1>Welcome <?php echo $name ?></h1>
 
-<button class="btn btn-success"><a href="song/insertSong">insertSong</a></button>
-<button class="btn btn-success"><a href="album/insertAlbum">insertAlbum</a></button>
-<button class="btn btn-success"><a href="album/updateAlbum">updateAlbum</a></button>
-<button class="btn btn-success"><a href="album/deleteAlbum">deleteAlbum</a></button>
+<button class="btn btn-success"><a href="albumAdmin/insertSong">insertSong</a></button>
 <!--<button class="btn btn-success"><a href="song/{song_id:\d+}">updateSong</a></button>-->
-<button class="btn btn-success"><a href="song/1">updateSong</a></button>
+<button class="btn btn-success"><a href="albumAdmin/1/updateSong/1">updateSong</a></button>
 <button class="btn btn-success"><a href="logout">logout</a></button>
 
