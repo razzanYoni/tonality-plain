de82fce29031b5cec97a413416a053014318a114
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-10-05 19:43:12 +0700

    fix: connect login

--- public/views/authorization/Login.php
+++ public/views/authorization/Login.php
@@ -8,11 +8,39 @@ use forms\Form;
 
 ?>
 
-<h1>Login</h1>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="/public/js/Login.js"></script>
+    <title>Login</title>
+</head>
+<body>
+    <div class="login-container">
+        <img class="logo" src="/public/assets/icons/logo.jpeg"></img>
+        <div class="login-title">Log in for Tonality</div>
+        <?php $form = Form::begin('', 'post', ['class' => 'login-form']); ?>
 
-<?php $form = Form::begin('', 'post') ?>
-<?php echo $form->field($model, 'username') ?>
-<?php echo $form->field($model, 'password')->passwordField() ?>
-<button class="btn btn-success">Submit</button>
-<?php Form::end() ?>
-<button class="btn btn-success"><a href="/register">Register</a></button>
+        <div class="login-quest">
+            <div class="form-label">Username</div>
+            <?php echo $form->field($model, 'username')->renderInput() ?>
+        </div>
+
+        <div class="login-quest">
+            <div class="form-label">Password</div>
+            <?php echo $form->field($model, 'password')->passwordField()->renderInput() ?>
+        </div>
+
+        <div class="btn-for-login">
+            <button class="login-btn">Log In</button>
+        </div>
+
+        <?php Form::end() ?>
+
+        <div class="sign-up-link">
+            Don't have an account? <a href="/register" class="signup-text">Sign up for Tonality</a>
+        </div>
+    </div>
+</body>
+</html>
