72671978df6174af1229f0fe324b2b7db2545928
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-10-07 10:23:31 +0700

    fix: kondisi admin dan not admin

--- public/components/AlbumCard.php
+++ public/components/AlbumCard.php
@@ -1,23 +1,38 @@
 <?php
+require_once ROOT_DIR . "src/cores/Application.php";
 
+use cores\Application;
 use models\AlbumModel;
 
 function AlbumCard(AlbumModel $album): string
 {
     $albumId = $album->get('album_id');
-    $coverUrl = $albumId->get('cover_url');
+    $coverUrl = $album->get('cover_url');
     $coverUrl = $coverUrl ? : 'public/assets/default-cover.jpg';
     $albumName = $album->get('album_name');
     $albumArtist = $album->get('artist');
 
-    return
-    <<<"EOT"
-    <a href="/album/$albumId" class="album-card">
-        <div class="album-info-container">
-            <img src=$coverUrl alt="album cover image" class="album-cover-image"/>
-            <div class="album-name">$albumName</div>
-            <div class="artist-name">$albumArtist</div>
-        </div>
-    </a>
-    EOT;
+    if (Application::$app->loggedUser->isAdmin()) {
+        return
+        <<<"EOT"
+        <a href="/albumAdmin/$albumId" class="album-card">
+            <div class="album-info-container">
+                <img src=$coverUrl alt="album cover image" class="album-cover-image"/>
+                <div class="album-name">$albumName</div>
+                <div class="artist-name">$albumArtist</div>
+            </div>
+        </a>
+        EOT;
+    } else {
+        return
+        <<<"EOT"
+        <a href="/album/$albumId" class="album-card">
+            <div class="album-info-container">
+                <img src=$coverUrl alt="album cover image" class="album-cover-image"/>
+                <div class="album-name">$albumName</div>
+                <div class="artist-name">$albumArtist</div>
+            </div>
+        </a>
+        EOT;
+    }
 }
