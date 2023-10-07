836e31650fe21e3ad2a4082eaa259d02347fcca9
razzanYoni <13521087@mahasiswa.itb.ac.id>
2023-10-07 15:07:51 +0700

    styling

--- public/views/song/InsertSong.php
+++ public/views/song/InsertSong.php
@@ -20,7 +20,10 @@ $form = new Form();
 <?php echo $form->field($model, 'artist') ?>
 <?php echo $form->field($model, 'song_number', ["min" => '1'])->numberField() ?>
 <?php echo $form->field($model, 'disc_number', ["min" => '1'])->numberField() ?>
-<?php echo $form->field($model, 'audio_url', ["id" => "input-file", "accept" => "audio/*"])->fileField() ?>
+<?php echo $form->field($model, 'audio_url',
+    ["id" => "input-file", "accept" => "audio/*"],
+    '<label for="input-file" class="custom-file-upload" id="file-label">Choose Your Song</label>'
+)->fileField() ?>
 <?php echo $form->field($model, 'duration')->numberField() ?>
 <button class="btn btn-success">Submit</button>
 <?php Form::end() ?>
