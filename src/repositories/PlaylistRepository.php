f3e70fee6269e264afa8b308a604e9127cff2cae
razzanYoni <13521087@mahasiswa.itb.ac.id>
2023-10-07 19:30:52 +0700

    add pagination and resolve error

--- src/repositories/PlaylistRepository.php
+++ src/repositories/PlaylistRepository.php
@@ -38,9 +38,42 @@ class PlaylistRepository extends BaseRepository
         return self::$instance;
     }
 
-    public function getPlaylistsByUserId($user_id)
+    public function getPlaylistsByUserId($order = null, $is_desc = false, $where = [], $where_like = '',$limit = ROWS_PER_PAGE, $offset = null): bool|array
     {
-        return $this->findAll(where: ["user_id" => $user_id]);
+        if ($where_like === '') {
+            $where_like = [];
+        } else {
+            $where_like = [
+                "playlist_name" => $where_like,
+            ];
+        }
+
+        return parent::findAll(
+            order: $order,
+            is_desc: $is_desc,
+            where: $where,
+            where_like:  $where_like,
+            limit: $limit,
+            offset: $offset
+        );
+    }
+
+    public function getCountPlaylistByUserId($where = [], $where_like = ''): int
+    {
+        if ($where_like === '') {
+            $where_like = [];
+        } else {
+            $where_like = [
+                "playlist_name" => $where_like,
+            ];
+        }
+
+        return parent::aggregate(
+            method: 'COUNT',
+            alias: 'count_playlists',
+            where: $where,
+            where_like:  $where_like
+        )['count_playlists'];
     }
 
     public function getPlaylistById($playlist_id)
@@ -48,8 +81,12 @@ class PlaylistRepository extends BaseRepository
         return $this->findOne(where: ["playlist_id" => $playlist_id]);
     }
 
-    public function getPlaylistByName($playlist_name)
+    public function IsPlaylistIsOwned($playlist_id)
     {
-        return $this->findAll(where: ["playlist_name" => $playlist_name]);
+        return $this->findOne(
+            where: [
+                "playlist_id" => $playlist_id,
+                "user_id" => Application::$app->loggedUser->getUserId()
+            ]);
     }
 }
