800caf59544e6cf26906298fd213d601f9feb0ec
razzanYoni <13521087@mahasiswa.itb.ac.id>
2023-10-07 20:24:13 +0700

    add styling to user

--- public/views/user/User.php
+++ public/views/user/User.php
@@ -4,19 +4,19 @@
 
 ?>
 
-<div class="song-table">
+<div class="user-table">
     <table>
         <thead>
-        <th class="song-number">#</th>
-        <th class="song-title">username</th>
-        <th class="song-duration">isAdmin</th>
+        <th class="user-number">#</th>
+        <th class="user-title">username</th>
+        <th class="user-duration">isAdmin</th>
         </thead>
         <tbody>
         <?php foreach ($users as $key => $user): ?>
-            <tr class="single-song">
-                <td class="song-number"><?php echo $key + 1; ?></td>
-                <td class="song-title"><?php echo $user['username']; ?></td>
-                <td class="song-duration-body"><?php echo $user['is_admin']; ?></td>
+            <tr class="single-user">
+                <td class="user-number"><?php echo $key + 1; ?></td>
+                <td class="user-title"><?php echo $user['username']; ?></td>
+                <td class="user-duration-body"><?php echo $user['is_admin']; ?></td>
                 <td><a href="/user/<?php echo $user['user_id']; ?>/deleteUser"><img src="public/assets/icons/trash-solid.svg" alt="Delete"></a></td>
             </tr>
         <?php endforeach; ?>
