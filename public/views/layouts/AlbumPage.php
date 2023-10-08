730528ca6f88bfd94afe7c8f06ae6760f6c0c0f8
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-10-08 13:55:19 +0700

    fix: add sort filter

--- public/views/layouts/AlbumPage.php
+++ public/views/layouts/AlbumPage.php
@@ -6,7 +6,7 @@
  * */
 require_once ROOT_DIR . "public/components/NavBar.php";
 require_once ROOT_DIR . "public/components/Pagination.php";
-
+require_once ROOT_DIR . "public/components/SortFilter.php";
 ?>
 
 <!DOCTYPE html>
@@ -15,12 +15,14 @@ require_once ROOT_DIR . "public/components/Pagination.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="/public/js/Search.js"></script>
+    <script src="/public/js/SortFilter.js"></script>
     <link rel="stylesheet" href="/public/css/Global.css" type="text/css">
     <link rel="stylesheet" href="/public/css/NavBar.css" type="text/css">
     <link rel="stylesheet" href="/public/css/Pagination.css" type="text/css">
 
     <link rel="stylesheet" href="/public/css/AlbumCard.css" type="text/css">
     <link rel="stylesheet" href="/public/css/AlbumPage.css" type="text/css">
+    <link rel="stylesheet" href="/public/css/SortFilter.css" type="text/css">
 
     <title>
         <?php
@@ -34,6 +36,10 @@ require_once ROOT_DIR . "public/components/Pagination.php";
 <body>
     <?php
         echo NavBar(currentPage: "Albums");
+    ?>
+
+    <?php
+        echo DropDown();
     ?>
 
     <div class="album-container">
