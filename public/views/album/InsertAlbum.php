4dd261abbbf81d8496d234f7d0414ca909c79476
Noel Simbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-10-08 12:53:58 +0700

    style: reformat  Insert Album

--- public/views/album/InsertAlbum.php
+++ public/views/album/InsertAlbum.php
@@ -11,40 +11,40 @@ use models\AlbumModel;
 
 ?>
 <div class="form-container">
-    <h1>
-       Add New Album
-    </h1>
+  <h1>
+    Add New Album
+  </h1>
 
     <?php $form = Form::begin('', 'post', ['class' => 'form-list'], enctype: "multipart/form-data"); ?>
 
-    <div class="album-quest">
-        <?php echo $form->field($model, 'album_name') ?>
-    </div>
+  <div class="album-quest">
+      <?php echo $form->field($model, 'album_name') ?>
+  </div>
 
-    <div class="album-quest">
-        <?php echo $form->field($model, 'artist') ?>
-    </div>
+  <div class="album-quest">
+      <?php echo $form->field($model, 'artist') ?>
+  </div>
 
-    <div class="album-quest">
-        <?php echo $form->field($model, 'genre') ?>
-    </div>
+  <div class="album-quest">
+      <?php echo $form->field($model, 'genre') ?>
+  </div>
 
-    <div class="album-quest">
-        <?php echo $form->field($model, 'cover_filename',
-            ["id" => "input-file", "accept" => "image/*"],
-            '<label for="input-file" class="custom-file-upload" id="file-label">Select a File</label>'
-        )->fileField();;
-        ?>
-    </div>
+  <div class="album-quest">
+      <?php echo $form->field($model, 'cover_filename',
+          ["id" => "input-file", "accept" => "image/*"],
+          '<label for="input-file" class="custom-file-upload" id="file-label">Select a File</label>'
+      )->fileField();
+      ?>
+  </div>
 
-    <div class="album-quest">
-        <?php echo $form->field($model, 'release_date', ["value" => '2001-01-01'])->dateField() ?>
-    </div>
+  <div class="album-quest">
+      <?php echo $form->field($model, 'release_date', ["value" => '2001-01-01'])->dateField() ?>
+  </div>
 
-    <div class="cancel-submit">
-        <button class="cancel-btn">Cancel</button>
-        <button class="add-btn" formaction="/albumAdmin/insertAlbum">Add Album</button>
-    </div>
+  <div class="cancel-submit">
+    <button class="cancel-btn">Cancel</button>
+    <button class="add-btn" formaction="/albumAdmin/insertAlbum">Add Album</button>
+  </div>
 
     <?php Form::end() ?>
 </div>
