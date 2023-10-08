3c01bb451b47bbb3b07071a50620455105a95d90
razzanYoni <13521087@mahasiswa.itb.ac.id>
2023-10-08 16:02:31 +0700

    refactor : default sort filter

--- public/components/SortFilter.php
+++ public/components/SortFilter.php
@@ -8,29 +8,70 @@ use repositories\AlbumRepository;
 function DropDown(): string
 {
     $genres = AlbumRepository::getInstance()->getAlbumGenres();
+
+    $default_genre = '<option disabled selected>Genre</option>';
+    if (isset($_GET['genre'])) {
+        $genre_from_get = $_GET['genre'];
+        $default_genre = '<option disabled>Genre</option>';
+    }
+
     $options = '';
     foreach ($genres as $genreData) {
         $genre = $genreData['genre'];
+        if (isset($genre_from_get) && $genre === $genre_from_get) {
+            $options .= '<option selected>' . $genre . '</option>';
+            continue;
+        }
         $options .= '<option>' . $genre . '</option>';
+    }
+
+    $default_sort = '<option disabled selected>Sort</option>';
+    if (isset($_GET['sort'])) {
+        $sort = $_GET['sort'];
+        $default_sort = '<option disabled>Sort</option>';
+
+        if ($sort === 'album_name') {
+            $default_sort .= '<option selected>Name</option>';
+            $default_sort .= '<option>Release Date</option>';
+        } else {
+            $default_sort .= '<option>Name</option>';
+            $default_sort .= '<option selected>Release Date</option>';
+        }
+    } else {
+        $default_sort .= '<option>Name</option>';
+        $default_sort .= '<option>Release Date</option>';
+    }
+
+    $default_desc = '<option disabled selected>Order</option>';
+    if (isset($_GET['is_desc'])) {
+        $desc = $_GET['is_desc'];
+        $default_desc = '<option disabled>Order</option>';
+
+        if ($desc === 'desc') {
+            $default_desc .= '<option selected>Descending</option>';
+            $default_desc .= '<option>Ascending</option>';
+        } else {
+            $default_desc .= '<option>Descending</option>';
+            $default_desc .= '<option selected>Ascending</option>';
+        }
+    } else {
+        $default_desc .= '<option>Descending</option>';
+        $default_desc .= '<option>Ascending</option>';
     }
 
     return <<<"EOT"
     <div class="container">
         <select class="filter">
-            <option disabled selected>Genre</option>
+            {$default_genre}
             {$options}
         </select>
 
         <select class="sort">
-            <option disabled selected>Sort</option>
-            <option>Name</option>
-            <option>Release Date</option>
+            {$default_sort}
         </select>
 
         <select class="desc">
-            <option disabled selected>Order</option>
-            <option>Descending</option>
-            <option>Ascending</option>
+            {$default_desc}
         </select>
     </div>
     EOT;
