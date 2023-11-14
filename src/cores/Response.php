78342f75e0ab776b9e4ed9b59fae4a9509f7e3e0
razzanYoni <13521087@mahasiswa.itb.ac.id>
2023-11-14 20:03:58 +0700

    add set header to response

--- src/cores/Response.php
+++ src/cores/Response.php
@@ -4,6 +4,11 @@ namespace cores;
 
 class Response
 {
+    public function setHeader(string $key, string $value)
+    {
+        header("$key: $value");
+    }
+
     public function statusCode(int $code)
     {
         http_response_code($code);
