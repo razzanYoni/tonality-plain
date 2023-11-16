0a605cb933c894133af7d8e5c5ffd35e26630590
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-11-17 04:09:29 +0700

    feat: prem album page

---
+++ public/views/premium/PremiumAlbum.php
@@ -0,0 +1,24 @@
+<?php
+
+/** @var $albums array
+ **/
+
+require_once ROOT_DIR . "src/models/PremiumAlbumModel.php";
+require_once ROOT_DIR . "src/controllers/PremiumAlbumController.php";
+require_once ROOT_DIR . "src/repositories/PremiumAlbumRepository.php";
+require_once ROOT_DIR . "public/components/AlbumCard.php";
+
+use models\PremiumAlbumModel;
+
+?>
+
+<div class="album-card-container" id="album-card-container">
+    <?php
+        foreach ($premiumAlbums as $album) {
+            $premiumAlbumModel = new PremiumAlbumModel();
+            $premiumAlbumModel->constructFromArray($album);
+            $premiumAlbumModels[] = $premiumAlbumModel;
+            echo AlbumCard($premiumAlbumModel);
+        }
+    ?>
+</div>
